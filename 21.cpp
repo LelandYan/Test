ec2a6a131c4e5bb1148d1a319bc4aad005d039fc
LelandYan <2721599586@qq.com>
2018-07-14 17:49:36 +0800

    第二天

--- 21.cpp
+++ 21.cpp
@@ -3,16 +3,16 @@
 #include <map>
 #include <vector>
 #include <algorithm>
+#define ll long long
 using namespace std;
 const int maxn = 30;
 void sm(vector<string>&obj,string b)
 {
     char a = b[b.size()-1];
     bool flag = false;
-    //cout <<"**" <<obj.size()<<" "<<a<<" "<<b <<"***\n";
     for(int i=0; i < obj.size(); i++)
     {
-        if(obj[i][obj[i].size()-1]==a)
+        if((obj[i][obj[i].size()-1])==a)
         {
             if(b.size()>obj[i].size())
             {
@@ -26,18 +26,18 @@ void sm(vector<string>&obj,string b)
 }
 int main()
 {
-    int n,num;
+    ll n,num;
     string strn,strp;
     while(cin>>n)
     {
         vector<string>phone[maxn];
-        map<string,int>mymap;
-        for(int i= 1; i <= n; i++)
+        map<string,ll>mymap;
+        for(ll i= 1; i <= n; i++)
         {
             cin >> strn >> num;
             bool flag = false;
             if(!mymap[strn]) mymap[strn] = i;
-            for(int j = 0; j < num; j++)
+            for(ll j = 0; j < num; j++)
             {
                 cin >> strp;
                 if(!flag)
@@ -46,7 +46,7 @@ int main()
                 sm(phone[mymap[strn]-1],strp);
             }
         }
-        map<string,int>::iterator iter;
+        map<string,ll>::iterator iter;
         cout << mymap.size()<<"\n";
         for(iter=mymap.begin(); iter!=mymap.end(); iter++)
         {
@@ -54,7 +54,7 @@ int main()
             sort(phone[iter->second-1].begin(),phone[iter->second-1].end());
             phone[iter->second-1].erase(unique(phone[iter->second-1].begin(), phone[iter->second-1].end()), phone[iter->second-1].end());
             cout << (phone[iter->second-1]).size() << " ";
-            for(int i = 0; i < (phone[iter->second-1]).size(); i++)
+            for(ll i = 0; i < (phone[iter->second-1]).size(); i++)
             {
 
                 cout << phone[iter->second-1][i] << " ";
