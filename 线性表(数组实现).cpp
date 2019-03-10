e157825f57991706d551154528f105d2a171e2e4
LelandYan <2721599586@qq.com>
2019-03-10 16:42:44 +0800

    array

--- 线性表(数组实现).cpp
+++ 线性表(数组实现).cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 const int MAXSIZE = 100;
@@ -21,6 +22,7 @@ int locateElem(SqList L,int e);
 int listInsert(SqList &L,int i,int e);
 int listDelete(SqList & L,int i);
 void display(SqList L);
+int getItem(SqList L,int e);
 void figure(int n,SqList & L)
 
 {
@@ -35,19 +37,23 @@ void figure(int n,SqList & L)
         cout << "7-----在线性表指定位置插入元素\n";
         cout << "8-----删除线性表指定位置元素\n";
         cout << "9-----显示线性表\n";
-        cout << "      退出：输入一个负数！";
+        cout << "10-----获取元素的位置\n";
+        cout << "      退出：输入一个负数！\n";
         cout << "请输入操作代码:";
         int n;
         cin >> n;
-        if( n < 0)return;
+        if( n < 0)
+            return;
         switch (n)
         {
         case 1:
             clearList(L);
             break;
         case 2:
-            if(is_Empty(L))cout << "该线性表为空\n";
-            else cout << "该线性表不为空\n";
+            if(is_Empty(L))
+                cout << "该线性表为空\n";
+            else
+                cout << "该线性表不为空\n";
             break;
         case 3:
             cout << "该线性表长度为：" <<get_Length(L) << endl;
@@ -58,48 +64,73 @@ void figure(int n,SqList & L)
             cin >> r;
             if(getElem(L,r,e))
                 cout << "该位置的元素为："<< e << endl;
-            else cout << "请输入正确的索引\n";
+            else
+                cout << "请输入正确的位置值\n";
             break;
 
         case 5:
             int k;
-            cout << "请输入要获取的前驱的索引值 :";
+            cout << "请输入要获取的前驱的位置 :";
             cin >> k;
             getbefore(L,k);
             break;
         case 6:
             int n;
-            cout << "请输入要获取的后驱的索引值 :";
+            cout << "请输入要获取的后驱的位置 :";
             cin >> n;
             getNext(L,n);
             break;
         case 7:
+            {cout << "插入的数据（位置，数据）：";
+            string str1;
+            cin>>str1;
+            int b = str1.find('(');
+            int c = str1.find(',');
+            int d = str1.find(')');
+            string n1 = str1.substr(1,c-1);
+            string n2 = str1.substr(c+1,d-c-1);
             int p,s;
-            cout << "请输入要插入的索引值: ";
-            cin >> p;
-            cout << "请输入要插入的元素的值：";
-            cin >> s;
+            p = atoi(n1.c_str());
+            s = atoi(n2.c_str());
             if(listInsert(L,p,s))
                 cout <<"插入成功\n";
             else
-                cout << "插入失败\n";
-            break;
+                cout << "插入不合法，插入失败\n";
+            break;}
         case 8:
             int z;
-            cout << "请输入要删除元素的索引值: ";
+            cout << "请输入要删除元素的位置: ";
             cin >>z;
-            if(listDelete(L,z))cout << "删除成功\n";
-            else cout << "删除失败\n";
+            if(listDelete(L,z))
+                cout << "删除成功\n";
+            else
+                cout << "删除失败\n";
             break;
         case 9:
             display(L);
             break;
+        case 10:
+            {int e;
+            cout << "请输入要定位的元素:";
+            cin >>e;
+            int index = getItem(L,e);
+            if(index)cout << "该元素的位置为:"<< index << endl;
+            else cout << "该元素不存在!\n";
+            break;}
         default:
             break;
         }
-    system("pause");
-    system("cls");
+        system("pause");
+        system("cls");
     }
+}
+int getItem(SqList L,int e)
+{
+    for(int i = 0; i < L.length; i++)
+    {
+        if(L.elem[i]==e){return i+1;break;}
+    }
+    return ERROR;
 }
 void display(SqList L)
 {
@@ -164,7 +195,8 @@ int listDelete(SqList & L,int i)
 void clearList(SqList & L)
 {
     int len = get_Length(L);
-    for(int i =len-1; i >= 0; i--){
+    for(int i =len; i > 0; i--)
+    {
         listDelete(L,i);
     }
     cout << "线性表已经清空\n";
@@ -182,22 +214,24 @@ int get_Length(SqList  L)
 void getNext(SqList L,int e)
 {
     int res = locateElem(L,e);
-    if(res==-1||(res+1) >= MAXSIZE)
-        cout << "请输入合法的索引\n";
+    if(res==-1||(res+1) >= MAXSIZE){
+        cout << "请输入合法的位置\n";
         return;
+    }
     int next;
     getElem(L,res+1,next);
-    cout << "该索引所对的后驱元素为：" << next << endl;
+    cout << "该位置所对的后驱元素为：" << next << endl;
 }
 void getbefore(SqList L,int e)
 {
     int res = locateElem(L,e);
-    if(res==-1||(res-1) <0 )
-        cout << "请输入合法的索引\n";
+    if(res==-1||(res-1) <0 ){
+        cout << "请输入合法的位置\n";
         return;
+    }
     int next;
     getElem(L,res-1,next);
-    cout << "该索引所对的前驱元素为：" << next << endl;
+    cout << "该位置所对的前驱元素为：" << next << endl;
 
 }
 int main()
@@ -206,7 +240,8 @@ int main()
 
     // 显示控制台的提示信息
     SqList lists;
-    if(initList(lists))cout << "线性表初始化完成: "<<endl;
+    if(initList(lists))
+        cout << "线性表初始化完成: "<<endl;
     figure(input_num,lists);
     return 0;
 }
